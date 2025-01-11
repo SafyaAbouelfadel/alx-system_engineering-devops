fdd9a68e18ddd5df41378825da20a4caac0d4ce7
Safyaabouelfadel <safia.abf@gmail.com>
2025-01-10 19:23:43 -0800

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,14 @@
 #install flack from pip3 using puppet
 
-package { 'Python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3',
+package {'install_flask':
+ensure   => '2.1.0',
+name     => 'flask'    ,
+provider => pip3,
 }
 
-package { 'Flack':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
 
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['Flask'],
+package {'install_werkzeug':
+ensure   => '2.1.1',
+name     => 'Werkzeug'    ,
+provider => pip3,
 }
