78a21efbb0c9ff53819b61ca74ef98d3b8243446
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-26 11:46:32 -0800

    modify file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# fixing apache 500 error
+# Changes phpp to php in a file that caused 500 server error
+service { 'apache2':
+  ensure => 'running',
+  enable => true,
+}
 
 exec { 'Fixing 500 error':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  command  => 'sudo sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   provider => shell,
 }
