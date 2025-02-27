4e39175383a3ffed559718ffcc26e0200c153711
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-26 17:33:44 -0800

    fix with apache2 restart

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,19 @@
-# Changes phpp to php in a file that caused 500 server error
+# Puppet manifest to fix Apache 500 error
 
-exec { 'modify_file':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/bin/:/bin/',
+exec { 'install_php_module':
+  command => '/usr/bin/apt-get install -y php libapache2-mod-php',
+  path    => ['/usr/bin', '/usr/sbin'],
+  onlyif  => 'test ! -f /usr/lib/apache2/modules/libphp7.so',
+}
+
+exec { 'set_permissions':
+  command => '/bin/chown -R www-data:www-data /var/www/html && /bin/chmod -R 755 /var/www/html',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'test $(stat -c "%U" /var/www/html) != "www-data"',
+}
+
+exec { 'restart_apache':
+  command => '/usr/sbin/service apache2 restart',
+  path    => ['/usr/sbin', '/bin', '/usr/bin'],
+  subscribe => Exec['install_php_module'],
 }
