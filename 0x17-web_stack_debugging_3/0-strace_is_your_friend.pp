6b096e81b3efbb32eaebc2b0d2822171e6b60835
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-26 17:45:33 -0800

    4 try

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,6 @@
 # Puppet manifest to fix Apache 500 error
 
-exec { 'install_php_module':
-  command => '/usr/bin/apt-get install -y php libapache2-mod-php',
-  path    => ['/usr/bin', '/usr/sbin'],
-  onlyif  => 'test ! -f /usr/lib/apache2/modules/libphp7.so',
-}
-
-exec { 'set_permissions':
-  command => '/bin/chown -R www-data:www-data /var/www/html && /bin/chmod -R 755 /var/www/html',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => 'test $(stat -c "%U" /var/www/html) != "www-data"',
-}
-
-exec { 'restart_apache':
-  command   => '/usr/sbin/service apache2 restart',
-  path      => ['/usr/sbin', '/bin', '/usr/bin'],
-  subscribe => Exec['install_php_module'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
