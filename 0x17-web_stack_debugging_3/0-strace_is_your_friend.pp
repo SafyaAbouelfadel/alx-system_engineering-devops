fc55cf4a652cbf6a2cd836b7b09e9687cf343195
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-26 20:15:57 -0800

    undo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# Puppet manifest to fix Apache 500 error
+# automated puppet fix (to find out why Apache is returning a 500 error)
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
